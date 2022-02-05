1dbea01cc080f5082ed4755d8c6c228653785e71
Masao Igarashi <masarashi@gmail.com>
2022-02-05 12:44:33 +0900

    2022020501

--- index.php
+++ index.php
@@ -4,46 +4,52 @@
         <div>
             <div class="hero">
                 <div class="hero__wrapper">
-                    <p class="hero__txt">笑顔と元気で承ります！</p>
+                    <p class="hero__txt">さいたま市・東京近郊で内装解体・不用品・産業廃棄物運搬・ゴミ回収</p>
                 </div>
             </div>
         </div>
 
         <section class="l-section">
             <div class="wrapper">
-                <h2 class="section-title --center --sp">さいたま市・東京近郊で内装解体・不用品・産業廃棄物運搬・ゴミ回収なら<br>I.M.Aクリーン産業にお任せください!</h2>
+                <h2 class="section-title --center --sp">I.M.Aクリーン産業にお任せください！</h2>
                 <div class="aboutus">
                     <div class="aboutus__item">
-                        <div class="aboutus__title">
-                            <p>笑顔と元気と対応力</p>
+                        <div class="aboutus__body">
+                            <div class="aboutus__title">
+                                <p>笑顔と元気と対応力</p>
+                            </div>
+                            <p class="aboutus__txt">
+                                笑顔と元気は必ず必要です。第一印象が悪いとうまく出来た仕事でも、あと味が悪くなります。まずはお客様に気持ちよく仕事を頼んでいただき、終わった後に気持ちよくありがとうって言われるように心がけています。
+                            </p>
                         </div>
-                        <p class="aboutus__txt">
-                            笑顔と元気は必ず必要です。第一印象が悪いとうまく出来た仕事でも、あと味が悪くなります。まずはお客様に気持ちよく仕事を頼んでいただき、終わった後に気持ちよくありがとうって言われるように心がけています。
-                        </p>
                         <div class="aboutus__img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/image/a.jpg" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/image/aboutus_image_01.jpeg" alt="">
                         </div>
                     </div>
                     <div class="aboutus__item">
-                        <div class="aboutus__title">
-                            <p>料金設定</p>
+                        <div class="aboutus__body">
+                            <div class="aboutus__title">
+                                <p>料金設定</p>
+                            </div>
+                            <p class="aboutus__txt">
+                                当社は必ず、現場を見て料金を出します。見積もりの際には、お客様と一緒に必ず取ります。万が一納得出来なければ断ってもらってかまいません。もちろん見積もりは無料です。無駄なものは省き、出来るだけお互い納得出来る料金を目指します。
+                            </p>
                         </div>
-                        <p class="aboutus__txt">
-                            当社は必ず、現場を見て料金を出します。見積もりの際には、お客様と一緒に必ず取ります。万が一納得出来なければ断ってもらってかまいません。もちろん見積もりは無料です。無駄なものは省き、出来るだけお互い納得出来る料金を目指します。
-                        </p>
                         <div class="aboutus__img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/image/a.jpg" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/image/aboutus_image_01.jpeg" alt="">
                         </div>
                     </div>
                     <div class="aboutus__item">
-                        <div class="aboutus__title">
-                            <p>細かな心使い</p>
+                        <div class="aboutus__body">
+                            <div class="aboutus__title">
+                                <p>細かな心使い</p>
+                            </div>
+                            <p class="aboutus__txt">
+                                一人ではどうすることも出来ないような、家具の移動。庭に生えた雑草など、やりたいけどなかなか出来ないような事など、お手伝いします。一人暮らしのお年寄りさんや、庭の草むしりが面倒な人、なんでもお気軽にご相談ください!心を込めてお手伝いします!
+                            </p>
                         </div>
-                        <p class="aboutus__txt">
-                            一人ではどうすることも出来ないような、家具の移動。庭に生えた雑草など、やりたいけどなかなか出来ないような事など、お手伝いします。一人暮らしのお年寄りさんや、庭の草むしりが面倒な人、なんでもお気軽にご相談ください!心を込めてお手伝いします!
-                        </p>
                         <div class="aboutus__img">
-                            <img src="<?php echo get_template_directory_uri(); ?>/image/a.jpg" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/image/aboutus_image_01.jpeg" alt="">
                         </div>
                     </div>
                 </div>
